2b82b209fe03288d560b5d0fa430c8fb7b5df401
hakisolos <165590443+hakisolos@users.noreply.github.com>
2025-08-14 11:41:51 +0100

    chore

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 /** @format */
 
 module.exports = {
-	BOT_TOKEN: '7986769252:AAFFjDUqQY2xTSl2Lp0qZP5GQbFHgbdWEnM',
+	BOT_TOKEN: '8014537287:AAGENfcPdLEN-S5Jy7b3Ee0P1fCVOAWa92w',
 	PREFIX: '?',
 	owner: 'H4KI',
 	API_URL: 'https://nikka-api.vercel.app',
